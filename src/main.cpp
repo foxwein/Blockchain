84b3924375ec7c40731e2338246a015e16974f29
1Kuso4ek1 <dimasalov2006@gmail.com>
2022-03-19 22:33:02 +0300

    askaks

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 
 int main()
 {
+    Json::Value root;
+    root["block"]["name"] = "hello world";
+    root["block"]["number"] = 123;
+
+    std::cout << root << std::endl;
     
     std::cout << GetRandomPrime(UINT32_MAX) << std::endl;
 }
