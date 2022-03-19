f1589ddb50b6f94a3bcd530433ba1cb4aa5f3fa4
1Kuso4ek1 <dimasalov2006@gmail.com>
2022-03-19 22:29:03 +0300

    jsoncpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 #include <Key.h>
 #include <RSA.h>
-#include <simdjson.h>
+#include <json/json.h>
 
 int main()
 {
-    simdjson::ondemand::parser parser;
-    simdjson::padded_string json = simdjson::padded_string::load("twitter.json");
-    simdjson::ondemand::document tweets = parser.iterate(json);
-    std::cout << uint64_t(tweets["search_metadata"]["count"]) << " results." << std::endl;
+    
     std::cout << GetRandomPrime(UINT32_MAX) << std::endl;
 }
